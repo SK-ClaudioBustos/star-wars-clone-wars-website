6193c918b37f81b7a1c6ab61e85041bc69db1061
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2023-11-08 21:27:40 -0300

    fix: delete anchor from footer

--- src/components/Foot.jsx
+++ src/components/Foot.jsx
@@ -6,8 +6,7 @@ function Foot() {
     return(
         <footer className="text-center">
             <p>
-            Hello there, the content of this page its not mine, just is for  demostration purpose - <a href="https:/github.com/SK-ClaudioBustos" target="_blank"
-				rel="noopener noreferrer">Claudio Bustos - Github <i className="bi bi-github"></i></a> 
+            Hello there, the content of this page its not mine, just is for  demostration purpose 
             </p>
         </footer>
     );
